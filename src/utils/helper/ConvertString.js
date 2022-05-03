0e29a49d3e0a33895838b4a3180cda759c9a6f88
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-05-03 23:47:41 +0700

    add helper convert string

---
+++ src/utils/helper/ConvertString.js
@@ -0,0 +1,13 @@
+const ConvertString20 = (string) => {
+  if (string != null && string.length >= 20) {
+    return string.substring(0, 20) + "...";
+  } else return string;
+};
+
+const ConvertString40 = (string) => {
+  if (string.length >= 40) {
+    return string.substring(0, 55) + "...";
+  } else return string;
+};
+
+export { ConvertString20, ConvertString40 };
