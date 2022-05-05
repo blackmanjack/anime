b583a6781745fd08d0e765de4e13ea52bb2be293
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-05-05 22:57:01 +0700

    add context provider

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Route, Link, BrowserRouter as Router, Routes } from "react-router-dom";
 import ListAnime from "../pages/anime";
 import AnimeDetail from "../pages/anime-detail";
+import ListCollection from "../pages/collection";
+import CollectionDetail from "../pages/collection-detail";
+import { AnimeContext } from "../pages/ContextAnime";
 
 const SwitchRouter = () => {
+  const [anime, setAnime] = useState([]);
+  const [formCollection, setFormCollection] = useState({});
+  const [listCollection, setListCollection] = useState([formCollection]);
+
+  //console.log("ListCollection", listCollection);
   return (
     <>
       <Router>
@@ -23,10 +32,26 @@ const SwitchRouter = () => {
           <hr />
         </>
 
-        <Routes>
-          <Route path="/" element={<ListAnime />} />
-          <Route path="/detail/:id" element={<AnimeDetail />} />
-        </Routes>
+        <AnimeContext.Provider
+          value={[
+            listCollection,
+            setListCollection,
+            anime,
+            setAnime,
+            formCollection,
+            setFormCollection,
+          ]}
+        >
+          <Routes>
+            <Route path="/" element={<ListAnime />} />
+            <Route path="/detail/:id" element={<AnimeDetail />} />
+            <Route path="/collection" element={<ListCollection />} />
+            <Route
+              path="/collection/detail/:id"
+              element={<CollectionDetail />}
+            />
+          </Routes>
+        </AnimeContext.Provider>
       </Router>
     </>
   );
