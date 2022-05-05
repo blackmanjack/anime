c1ed323ee99aeeb614fcaea4bc36d265024f7b0c
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-05-05 22:56:27 +0700

    fix bug

--- src/pages/anime.js
+++ src/pages/anime.js
@@ -1,17 +1,13 @@
 import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 import { GET_ALL_DATA } from "../utils/graphql/query";
 import { ConvertString20 } from "../utils/helper/ConvertString";
 
 const ListAnime = () => {
-  //   const [listData, setListData] = useState([]);
   const { loading, error, data } = useQuery(GET_ALL_DATA);
 
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-
-  //   console.log("DATA =>", data.Page.media);
-
-  //   setListData(data.Page.media);
 
   return (
     <>
@@ -20,7 +16,8 @@ const ListAnime = () => {
           {(data.Page.media || []).map((item) => (
             //grid
             <div key={item.id} className="card-anime">
-              <a href={`/detail/` + item.id}>
+              <Link to={`/detail/` + item.id}>
+                {/* <a href={`/detail/` + item.id}> */}
                 <img src={item.coverImage.large} alt={item.title.english}></img>
                 <div className="text-card">
                   <div>
@@ -29,7 +26,8 @@ const ListAnime = () => {
                     </p>
                   </div>
                 </div>
-              </a>
+                {/* </a> */}
+              </Link>
             </div>
           ))}
         </div>
