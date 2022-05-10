6af755429da0b1033b0b90712de516658c1286ce
blackmanjack <faiz_m17@apps.ipb.ac.id>
2022-05-10 22:56:50 +0700

    update UI component collection

--- src/component/collection.js
+++ src/component/collection.js
@@ -1,15 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AnimeContext } from "../pages/ContextAnime";
 
-const IncludeCollection = ({ item, index }) => {
-  return (
-    <>
-      <div>
-        <Link to={`/collection/detail/` + item.name}>
-          <div key={index}>{item.name}</div>
-        </Link>
-      </div>
-    </>
-  );
+const IncludeCollection = () => {
+  const [anime] = useContext(AnimeContext);
+  return (anime || []).map((item, index) => (
+    <div>
+      <Link to={`/collection/detail/` + item.name}>
+        <div key={index}>{item.name}</div>
+      </Link>
+    </div>
+  ));
 };
 
 export default IncludeCollection;
